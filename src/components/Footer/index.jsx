7c2c77bf2cac90900b7a028d30709c263e88fc0b
Raymond Wang <rawang@ntdev.microsoft.com>
2019-10-27 22:15:54 -0700

    changed favicon and footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,26 +7,26 @@ export default function Footer() {
       <div className="container-fluid">
         <div className="row justify-content-around">
           <div className="col-8 col-md-5">
-            <h5 className={styles.title}>DiamondSeaTurtle</h5>
+            <h5 className={styles.title}>Status:</h5>
             <p className={styles.description}>
-              This is placeholder text. Your web app description goes here.
+              This website is under construction
             </p>
           </div>
           <div className="col-2">
             <ul className="list-unstyled">
               <li>
-                <a className={styles.footerlink} href="/">
-                  Example Link
+                <a className={styles.footerlink} href="https://bsd405.org/nhs/">
+                  School
                 </a>
               </li>
               <li>
-                <a className={styles.footerlink} href="/">
-                  Example Link
+                <a className={styles.footerlink} href="https://github.com/diamondseaturtle/ProductionWeb">
+                  GitHub
                 </a>
               </li>
               <li>
-                <a className={styles.footerlink} href="/">
-                  Example Link
+                <a className={styles.footerlink} href="https://www.youtube.com/channel/UCxe4BmIHCR2zF6AO1-oMlgw">
+                  YouTube
                 </a>
               </li>
             </ul>
